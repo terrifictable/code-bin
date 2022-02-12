9b4526e88fc050b7563ddb28c2362cb81e664247
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-02-12 12:40:08 +0100

    shity code, make & write files with post requests

---
+++ fileapi.py
@@ -0,0 +1,37 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const fs = require('fs');
+
+const api = express();
+api.use(bodyParser.json());
+
+api.listen(5000, () => {
+    console.log("API up and running (http://localhost:5000)");
+});
+
+let workingDir = "./data/"
+
+
+api.post("/add", (req, res) => {
+    console.log(req.body);
+    let name = req.body['name'];
+    let content = req.body['content'];
+    let path = req.body['path'];
+    let newpath = path.replace(' ', "_") + "/"
+
+    if (newpath == "") { newpath = "/" }
+    let tmppath = workingDir + newpath + name
+    let finalpath = tmppath.replace("//", "/")
+
+    if (name != "" || name != None) {
+        fs.mkdir(workingDir + path, () => { "succsess" })
+        if (fs.existsSync(finalpath)) {
+            res.send('File already exists')
+        }
+        fs.writeFileSync(finalpath, content)
+        try {
+            res.send("succsess");
+        } catch { "" }
+    }
+    try { res.send('Invalid name') } catch { "" }
+});
