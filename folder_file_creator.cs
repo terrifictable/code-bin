5d692c05baedad8e5b9816297c30cebc9a2402b1
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-02-21 23:55:30 +0100

    creates subfolders in all folders in a folder
    that were many folders in one sentence

---
+++ folder_file_creator.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IO;
+using System.Threading;
+
+namespace file_creator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dirPath = @"PATH";
+            string[] folders = GetSubFolders(dirPath);
+
+
+            foreach (string folder in folders) {
+                if (Directory.Exists(folder)) {
+                    CreateFiles(folder, "SUBFOLDERNAME", "FILENAME.EXTENTION", "FILECONTENT", 10);
+                }
+            }
+
+        }
+
+        public static string[] GetSubFolders(string path) {
+            // Returns Sub Folders
+            string[] folders = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
+            return folders;
+        }
+
+        public static bool CreateFiles(string path, string foldername, string filename, string filecontent, int amount) {
+            // Returns true on succsess otherwise false
+
+            try {
+                string pathString = System.IO.Path.Combine(path, foldername);
+                System.IO.Directory.CreateDirectory(pathString);
+
+                for (int i=0; i<amount; i++) {
+                    string filepath = pathString + "/" + filename.Split(".")[0] + i + "." + filename.Split(".")[1];
+
+                    File.WriteAllText(filepath, filecontent);
+                }
+            } catch { return false; }
+
+            return true;
+        }
+    }
+}
