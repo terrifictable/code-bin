715caefb3f6ee8400e31a147cd664dfc336523ba
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-03-04 23:26:57 +0100

    execute commands like in terminal and print output

--- exec_term.c
+++ exec_term.c
@@ -3,22 +3,7 @@
 
 void exec_cmd(char *command)
 {
-    FILE *fptr;
-    char c;
-
     system(command);
-    fptr = fopen("temp.txt", "r");
-
-    while (1)
-    {
-        c = fgetc(fptr);
-        if (c != EOF)
-            printf("%c", c);
-        else
-            break;
-    }
-    fclose(fptr);
-    remove("temp.txt");
 }
 
 int main(int argc, char *argv[])
