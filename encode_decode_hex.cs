1753759b048d1b59c1b29adca05a5a567fabbd20
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-03-04 22:37:42 +0100

    shit code to encode or decode hex with a simple ui

---
+++ encode_decode_hex.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Text;
+
+
+namespace test
+{
+    class Program
+    {
+        public static bool url = false;
+
+        static void Main(string[] args)
+        {
+            string str;
+            string hex;
+            
+
+
+            Console.Clear();
+            Console.WriteLine(" [1] Encode Hex\n [2] Decode Hex\n [3] Settings");
+            Console.Write("Option: ");
+            string option = Console.ReadLine();
+
+            if (option == "1") {
+                Console.Write("\nString: ");
+                str = Console.ReadLine();
+
+                if (url == true) {
+                    Console.WriteLine("Hex: https://%" + StrToHex(str).Replace(" ", "%"));
+                    Console.ReadKey();
+                } else {
+                    Console.WriteLine("Hex: " + StrToHex(str));
+                    Console.ReadKey();
+                }
+
+            } else if (option == "2") {
+                Console.Write("\nHex: ");
+                hex = Console.ReadLine();
+
+                Console.WriteLine("String: " + HexToStr(hex));
+                Console.ReadKey();
+
+            } else if (option == "3") {
+                Console.WriteLine("\n\n---SETTINGS---");
+                Console.WriteLine(" URL Encoding: " + url.ToString());
+                Console.WriteLine("--------------\n");
+
+                Console.Write("Toggle 'URL Encoding' [y/n]");
+                string toggle = Console.ReadLine();
+
+                if (toggle.ToLower() == "y") {
+                    url = true;
+
+                    Console.WriteLine("\nPress any key to continue");
+                    Console.ReadKey();
+                    Main(args);
+                } else {
+                    Console.WriteLine("\nPress any key to continue");
+                    Console.ReadKey();
+                    Main(args);
+                }
+
+            } else {
+                Console.WriteLine("Invalid Input\n  Press [ENTER] to return");
+                Console.ReadKey();
+                Main(args);
+            }
+        }
+
+        static string StrToHex(string str)
+        {
+            byte[] bytearray = Encoding.Default.GetBytes(str);
+            var hexString = BitConverter.ToString(bytearray);
+
+            return hexString.Replace("-", " ").ToString();
+        }
+
+        static string HexToStr(string hex)
+        {
+            hex = hex.Replace(" ", "").Replace("-", "");
+            byte[] raw = new byte[hex.Length / 2];
+            for (int i = 0; i < raw.Length; i++) {
+                raw[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
+            }
+
+            return Encoding.ASCII.GetString(raw).ToString();
+        }
+    }
+}
