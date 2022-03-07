baf68fb562c58335036e3c9d7adbd1ad3e90d80b
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-03-07 19:53:36 +0100

    execute python, js, c/cpp and shell commands/code
    added comments explaining the code

--- execute.c
+++ execute.c
@@ -3,66 +3,62 @@
 
 void exec_cmd(char *command)
 {
-    system(command);
+    system(command); // run terminal command
 }
 
 void exec_js(char *code)
 {
-    FILE *fptr;
-    fptr = fopen("./temp.js", "w");
+    FILE *fptr = fopen("./temp.js", "w"); // create and open temp file
 
-    fprintf(fptr, "%s\n", code);
-    fclose(fptr);
+    fprintf(fptr, "%s\n", code); // write code
+    fclose(fptr);                // close temp file
 
-    system("node ./temp.js");
-    remove("temp.js");
+    system("node ./temp.js"); // execute temp file
+    remove("temp.js");        // remove temp file
 }
 
 void exec_c(char *code)
 {
-    FILE *fptr;
-    fptr = fopen("./temp.c", "w");
+    FILE *fptr = fopen("./temp.c", "w"); // create and open temp file
 
-    fprintf(fptr, "%s\n", code);
-    fclose(fptr);
+    fprintf(fptr, "%s\n", code); // write code
+    fclose(fptr);                // close temp file
 
-    system("gcc ./temp.c -o temp");
-    remove("temp.c");
-    system("temp.exe");
-    remove("temp.exe");
+    system("gcc ./temp.c -o temp"); // compile temp file
+    remove("temp.c");               // remove temp file
+    system("temp.exe");             // execute compiled code
+    remove("temp.exe");             // remove compiled code file
 }
 
 void exec_cpp(char *code)
 {
-    FILE *fptr;
-    fptr = fopen("./temp.cpp", "w");
+    FILE *fptr = fopen("./temp.cpp", "w"); // create and open temp file
 
-    fprintf(fptr, "%s\n", code);
-    fclose(fptr);
+    fprintf(fptr, "%s\n", code); // write code
+    fclose(fptr);                // close temp file
 
-    system("gcc ./temp.cpp -o temp");
-    remove("temp.cpp");
-    system("temp.exe");
-    remove("temp.exe");
+    system("gcc ./temp.cpp -o temp"); // compile temp file
+    remove("temp.cpp");               // remove temp file
+    system("temp.exe");               // execute temp executable
+    remove("temp.exe");               // remove temp executable
 }
 
 void exec_py(char *code)
 {
-    FILE *fptr;
-    fptr = fopen("./temp.py", "w");
+    FILE *fptr = fopen("./temp.py", "w"); // create and open temp file
 
-    fprintf(fptr, "%s\n", code);
-    fclose(fptr);
+    fprintf(fptr, "%s\n", code); // write temp file
+    fclose(fptr);                // close temp file
 
-    system("python3 temp.py");
-    remove("temp.py");
+    system("python3 temp.py"); // execute temp file
+    remove("temp.py");         // remove temp file
 }
 
 int main(int argc, char *argv[])
 {
-    exec_cmd("echo Hello World! { teminal }");
-    exec_js("console.log(\"Hello World! { Node.js }\")");
-    exec_c("#include <stdio.h>\nint main() { printf(\"Hello World! { C }\"); return 0; }");
-    exec_cpp("#include <stdio.h>\nint main() { printf(\"Hello World! { C++ }\"); }");
-    exec_py("print(\"Hello World! { Python }\")");
+    exec_cmd("echo Hello World! { Teminal }");                                           // Execute stuff from/in terminal
+    exec_js("console.log(\"Hello World! { Node.js }\")");                                // Execute Node.js code
+    exec_c("#include <stdio.h>\nint main() { printf(\"Hello World! { C }\\n\"); }");     // Execute C code (why would you ever want to use this?)
+    exec_cpp("#include <stdio.h>\nint main() { printf(\"Hello World! { C++ }\\n\"); }"); // Execute C++ Code
+    exec_py("print(\"Hello World! { Python }\")");                                       // Execute Python Code
 }
