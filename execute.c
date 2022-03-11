d4c5e6b6835fed417316abf72fbcf340d0b0a486
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-03-11 17:22:29 +0100

    execute python, js, c/cpp, java, kotlin, shell

--- execute.c
+++ execute.c
@@ -43,6 +43,30 @@ void exec_cpp(char *code)
     remove("temp.exe");               // remove temp executable
 }
 
+void exec_java(char *code)
+{
+    FILE *fp = fopen("./temp.java", "w"); // create and open temp file
+
+    fprintf(fp, "%s\n", code); // write code
+    fclose(fp);                // close temp file
+
+    system("java ./temp.java"); // execute temp file
+    remove("temp.java");        // remove temp file
+}
+
+void exec_kotlin(char *code)
+{
+    FILE *fp = fopen("./temp.kt", "w"); // create an open temp file
+
+    fprintf(fp, "%s\n", code); // write code
+    fclose(fp);                // close temp file
+
+    system("kotlinc temp.kt -include-runtime -d ./temp.jar"); // compile temp file  (takes some time)
+    remove("temp.kt");                                        // remove temp file
+    system("java -jar ./temp.jar");                           // execute compiled temp file
+    remove("temp.jar");                                       // remove compiled temp file
+}
+
 void exec_py(char *code)
 {
     FILE *fptr = fopen("./temp.py", "w"); // create and open temp file
@@ -56,9 +80,11 @@ void exec_py(char *code)
 
 int main(int argc, char *argv[])
 {
-    exec_cmd("echo Hello World! { Teminal }");                                           // Execute stuff from/in terminal
-    exec_js("console.log(\"Hello World! { Node.js }\")");                                // Execute Node.js code
-    exec_c("#include <stdio.h>\nint main() { printf(\"Hello World! { C }\\n\"); }");     // Execute C code (why would you ever want to use this?)
-    exec_cpp("#include <stdio.h>\nint main() { printf(\"Hello World! { C++ }\\n\"); }"); // Execute C++ Code
-    exec_py("print(\"Hello World! { Python }\")");                                       // Execute Python Code
+    exec_cmd("echo Hello World! { Teminal }");                                                                                            // Execute stuff from/in terminal
+    exec_js("console.log(\"Hello World! { Node.js }\")");                                                                                 // Execute Node.js code
+    exec_c("#include <stdio.h>\nint main() { printf(\"Hello World! { C }\\n\"); }");                                                      // Execute C code (why would you ever want to use this?)
+    exec_cpp("#include <stdio.h>\nint main() { printf(\"Hello World! { C++ }\\n\"); }");                                                  // Execute C++ Code
+    exec_java("public class temp {\n public static void main(String[] args) {\n System.out.println(\"Hello World! { Java }\"); \n} \n}"); // Execute Java Code
+    exec_kotlin("fun main() {\n println(\"Hello World! { Kotlin }\"); \n}");                                                              // Execute Kotlin Code (this will take some time)
+    exec_py("print(\"Hello World! { Python }\")");                                                                                        // Execute Python Code
 }
