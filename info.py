767459ecef34e492e0c00000eb1d08aa30d20acb
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-04-21 02:58:49 +0200

    displays some info about your operating system
    example: https://cdn.discordapp.com/attachments/944354446337507358/966502561060360233/unknown.png
    
    LINUX ONLY (except you can get the commands to work on windows/mac too)

--- info.py
+++ info.py
@@ -4,9 +4,10 @@ import psutil
 import time
 import os
 
+
 def get_wm():
     output = subprocess.run(["wmctrl", "-m"], text=True,
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if output.stderr:
         return output.stderr
     else:
@@ -15,13 +16,15 @@ def get_wm():
 
         return res
 
+
 def get_sh():
     return psutil.Process(os.getpid()).parent().name()
+
 
 def get_system():
     os = platform.system()
     version = platform.release()
-    
+
     new_v = ""
     version = version.replace("5.17.3-", "").replace("1-1", "")
     for x, char in enumerate(version):
@@ -32,18 +35,20 @@ def get_system():
             new_v += char
     return new_v + "-" + os
 
+
 def get_colors():
     return "\033[91m\uf05b \033[96m\uf05b \033[93m\uf05b \033[94m\uf05b \033[95m\uf05b \033[90m\uf05b \033[0m"
 
+
 def get_time():
     return time.strftime("%H:%M")
+
 
 def main():
     try:
         wm = get_wm()
         sh = get_sh()
-        if os.name == "nt":
-            os.system("mode 12 12")
+        
         os.system("clear")
         print("  " + get_system())
         print(" ------------")
@@ -54,6 +59,7 @@ def main():
         print("  | " + get_time() + " | ")
     except KeyboardInterrupt:
         exit(0)
+
 
 while True:
     main()
