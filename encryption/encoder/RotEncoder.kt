ae27fce638c86c1cf2fb67da3025cd236ad75c61
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-04-27 15:49:59 +0200

    Create RotEncoder.kt

---
+++ encryption/encoder/RotEncoder.kt
@@ -0,0 +1,27 @@
+package encoder
+
+class RotEncoder {
+    companion object {
+        private const val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!§$%&/()=?"
+
+        fun RotEncode(stringIn: String, offset: Int): String {
+            val stringOut = StringBuilder()
+            for (i in stringIn.indices) {
+                try {
+                    var index: Int = alphabet.indexOf(stringIn[i]) + offset
+                    if (index > alphabet.length) {
+                        index -= alphabet.length
+                    }
+                    if (stringIn[i] != ' ') {
+                        stringOut.append(alphabet[index])
+                    } else {
+                        stringOut.append(" ")
+                    }
+                } catch (e: Exception) {
+                    stringOut.append(stringIn[i])
+                }
+            }
+            return stringOut.toString()
+        }
+    }
+}
