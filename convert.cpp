3d1967121a1dc4f871b9166361e83766fae0d238
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-05-03 09:56:35 +0200

    convert char[] to String

---
+++ convert.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+
+
+String convert(char chIn[])
+{
+    String result = "";
+    for (unsigned int i = 0; i < strlen(chIn); i++) 
+    {
+        result += chIn[i];
+    }
+    return result;
+}
